0f31cd65461c2388f8b4cb717a3e51047e919c16
MrCatnip <doe69772@gmail.com>
2025-02-05 14:20:02 +0200

    Finished !

--- index.ts
+++ index.ts
@@ -1,15 +1,56 @@
 import express, { Request, Response } from 'express';
+import { Client, GatewayIntentBits, TextChannel } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+// Login the bot
+client.login(process.env.DISCORD_TOKEN);
+
+type Alert = {
+  pattern: 'Red First' | 'Green First';
+  candle: {
+    closeArray: number[];
+    openArray: number[];
+    highArray: number[];
+    lowArray: number[];
+    timestampArray: number[];
+  };
+  symbol: string;
+  timeframe: string;
+};
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Root endpoint
-app.post('/', (req: Request, res: Response) => {
-  console.log('Received JSON:', req.body);
-  res.json({ message: 'JSON received successfully', data: req.body });
+// @ts-expect-error shut up
+app.post('/', async (req: Request, res: Response) => {
+  const receivedAlert = req.body as Alert;
+  try {
+    const channel = (await client.channels.fetch(
+      process.env.DISCORD_CHANNEL_ID || '',
+    )) as TextChannel;
+
+    if (!channel) {
+      return res.status(500).json({ error: 'Channel not found' });
+    }
+
+    await channel.send(
+      `${receivedAlert.symbol}/${receivedAlert.timeframe}: ${receivedAlert.pattern}`,
+    );
+    return res.json({ message: 'Message sent to Discord!' });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    return res.status(500).json({ error: 'Failed to send message' });
+  }
 });
 
 app.listen(PORT, () => {
