0cc35a772d761090b1199f2621945dfdcb513880
MrCatnip <doe69772@gmail.com>
2025-02-05 14:40:13 +0200

    Finished 4 real

--- index.ts
+++ index.ts
@@ -14,6 +14,24 @@ const client = new Client({
 // Login the bot
 client.login(process.env.DISCORD_TOKEN);
 
+type Timeframe =
+  | '1min'
+  | '3min'
+  | '5min'
+  | '15min'
+  | '30min'
+  | '45min'
+  | '1h'
+  | '2h'
+  | '3h'
+  | '4h'
+  | '6h'
+  | '8h'
+  | '12h'
+  | '1D'
+  | '1W'
+  | '1M';
+
 type Alert = {
   pattern: 'Red First' | 'Green First';
   candle: {
@@ -24,7 +42,7 @@ type Alert = {
     timestampArray: number[];
   };
   symbol: string;
-  timeframe: string;
+  timeframe: Timeframe;
 };
 
 // Middleware to parse JSON requests
@@ -34,18 +52,23 @@ app.use(express.json());
 // @ts-expect-error shut up
 app.post('/', async (req: Request, res: Response) => {
   const receivedAlert = req.body as Alert;
+  const { symbol, timeframe, pattern } = receivedAlert;
   try {
     const channel = (await client.channels.fetch(
-      process.env.DISCORD_CHANNEL_ID || '',
+      (timeframe === '4h'
+        ? process.env.DISCORD_CHANNEL_ID_4h
+        : timeframe === '1D'
+          ? process.env.DISCORD_CHANNEL_ID_1D
+          : timeframe === '1W'
+            ? process.env.DISCORD_CHANNEL_ID_1W
+            : process.env.DISCORD_CHANNEL_ID_REST) as string,
     )) as TextChannel;
 
     if (!channel) {
       return res.status(500).json({ error: 'Channel not found' });
     }
 
-    await channel.send(
-      `${receivedAlert.symbol}/${receivedAlert.timeframe}: ${receivedAlert.pattern}`,
-    );
+    await channel.send(`${symbol}/${timeframe}: ${pattern}`);
     return res.json({ message: 'Message sent to Discord!' });
   } catch (error) {
     console.error('Error sending message:', error);
